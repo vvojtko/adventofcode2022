6e8f0eb6bd2db006824752d7fb7ed38830435f10
wojtek s <wojciech.sowinski@windowslive.com>
2022-12-02 12:53:28 +0100

    completed task no. 2 and change code for task no. 1 to make it short

--- day02/file.py
+++ day02/file.py
@@ -40,13 +40,74 @@ def calculateScore(opponentMove, yourMove):
             tempScore = tempScore + 3
     return tempScore
 
+opponentScoreDict = {"A":1,"B":2,"C":3}
+yourScoreDict = {"X":1, "Y":2,"Z":3}
+#below is a dictionary that is used to output the outcome of a round
+#with the following order:
+# 1st letter = opponent's move
+# 2nd letter = yours move
+
+rulesDictOpponentVsYou= {
+    "XA":"draw",
+    "AX":"draw",
+    "BY":"draw",
+    "YB":"draw",
+    "CZ":"draw",
+    "ZC":"draw",
+    "AY":"win",
+    "AZ":"lose",
+    "BX":"lose",
+    "BZ":"win",
+    "CX":"win",
+    "CY":"lose"
+}
+
+def calculateScoreV2(opponentMove, yourMove):
+    if(rulesDictOpponentVsYou.get(opponentMove+yourMove)=="lose"):
+        return yourScoreDict.get(yourMove)
+    elif(rulesDictOpponentVsYou.get(opponentMove+yourMove)=="win"):
+        return yourScoreDict.get(yourMove)+6
+    else:
+        return yourScoreDict.get(yourMove)+3
+
+
+def calculateScorePartTwo(opponentMove, roundEnding):
+        match roundEnding:
+            case "Y": #it's a DRAW
+                return opponentScoreDict.get(opponentMove)+3
+            case "X": #it's a LOSE
+                match opponentMove:
+                    case "A":
+                        return 3
+                    case "C":
+                        return 2
+                    case "B":
+                        return 1
+            case "Z": #it's a WIN
+                match opponentMove:
+                    case "C":
+                        return 7
+                    case "B":
+                        return 9
+                    case "A":
+                        return 8
+
+
 filename = "input.txt"
 with open(filename) as file:
     totalScore = 0
+    totalScoreV2 = 0
+    totalScorePartTwo = 0
     for line in file:
         fight = line.rstrip().split(" ")
         #print(fight[0], " vs ",fight[1])
         score = calculateScore(fight[0], fight[1])
-        totalScore = totalScore + score
-        print("Op move: ", fight[0], "- Your move: ", fight[1], " --  Round score: ", score)
+        scoreV2 = calculateScoreV2(fight[0], fight[1])
+        totalScoreV2 = totalScoreV2 + scoreV2
+        scorePartTwo = calculateScorePartTwo(fight[0], fight[1])
+        totalScore = totalScore + score #total score sum for part 1
+        totalScorePartTwo = totalScorePartTwo + scorePartTwo #total socre sum for part 2
+        #print("Op move: ", fight[0], "- Your move: ", fight[1], " --  Round score: ", score)
     print("Total score: ", totalScore)
+    print("Total score: ", totalScoreV2)
+    print("Total score part two: ", totalScorePartTwo)
