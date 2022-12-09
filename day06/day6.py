cb359c3ea5de4885c4768b4352d3f604bbfad30b
wojtek s <wojciech.sowinski@windowslive.com>
2022-12-09 12:04:47 +0100

    completed task 2 for day 6

--- day06/day6.py
+++ day06/day6.py
@@ -5,13 +5,15 @@ with open(filename) as file:
         #print(code)
         code = list(code)
         counter = 0
+        counterRepeat = 0 #counter for repeating checking same characters in next loops
         markerDict = {}
         print(code[:4])
+        distinctCharacterNumber = 4
         run = True
         while run == True:
-            for each in range(counter, counter+4):
-                #print(code[counter])
-                if len(markerDict)==4:
+            counter=counterRepeat
+            for each in range(counter, counter+distinctCharacterNumber):
+                if len(markerDict)==distinctCharacterNumber:
                     run = False
                 elif markerDict.get(code[counter]) == None:
                     print(code[counter], " doesn't exist therefore adding it")
@@ -19,6 +21,38 @@ with open(filename) as file:
                     counter=counter+1
                 elif markerDict.get(code[counter]) == "exists":
                     print(code[counter], "exists already in marker Dict therefore breaking the loop")
+                    counterRepeat=counterRepeat+1
+                    print("counter: ",counter)
+                    print("counter repeat: ", counterRepeat)
                     markerDict = {}
                     break
-        print(counter)
+        print(counter+distinctCharacterNumber)
+
+with open(filename) as file:
+    for line in file:
+        code = line.rstrip()
+        #print(code)
+        code = list(code)
+        counter = 0
+        counterRepeat = 0 #counter for repeating checking same characters in next loops
+        markerDict = {}
+        print(code[:4])
+        distinctCharacterNumber = 14
+        run = True
+        while run == True:
+            counter=counterRepeat
+            for each in range(counter, counter+distinctCharacterNumber):
+                if len(markerDict)==distinctCharacterNumber:
+                    run = False
+                elif markerDict.get(code[counter]) == None:
+                    print(code[counter], " doesn't exist therefore adding it")
+                    markerDict[code[counter]] = "exists"
+                    counter=counter+1
+                elif markerDict.get(code[counter]) == "exists":
+                    print(code[counter], "exists already in marker Dict therefore breaking the loop")
+                    counterRepeat=counterRepeat+1
+                    print("counter: ",counter)
+                    print("counter repeat: ", counterRepeat)
+                    markerDict = {}
+                    break
+        print(counter+distinctCharacterNumber)
